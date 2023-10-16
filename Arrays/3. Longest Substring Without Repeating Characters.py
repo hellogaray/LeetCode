e608c345d468eeed602cc41657c35df802eca9e8
hellogaray <ant.zeron@gmail.com>
2023-10-16 11:34:27 +0900

    Problem 3 - Arrays

---
+++ Arrays/3. Longest Substring Without Repeating Characters.py
@@ -0,0 +1,22 @@
+"""
+Given a string s, find the length of the longest substring without repeating characters.
+
+Example:
+    Input: s = "abcabcbb"
+    Output: 3
+    Explanation: The answer is "abc", with the length of 3.
+"""
+
+
+def lengthOfLongestSubstring(s):
+    unique_chars = set()
+    max_length = 0
+    left_boundary = 0
+    for right_boundary in range(len(s)):
+        while s[right_boundary] in unique_chars:
+            unique_chars.remove(s[left_boundary])
+            left_boundary += 1
+        unique_chars.add(s[right_boundary])
+        current_length = right_boundary - left_boundary + 1
+        max_length = max(max_length,current_length )
+    return max_length
