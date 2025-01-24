cb2066b565df8231715c5889d00faa6afb8d1228
hellogaray <137473678+hellogaray@users.noreply.github.com>
2025-01-24 12:00:00 +0900

    Study

---
+++ Arrays/58. Length of Last Word.py
@@ -0,0 +1,10 @@
+s = "Hello World"
+def lengthOfLastWord(s):
+
+
+s = "hello world"
+print(lengthOfLastWord(s))
+s = "   fly me   to   the moon  "
+print(lengthOfLastWord(s))
+s = "luffy is still joyboy"
+print(lengthOfLastWord(s))
