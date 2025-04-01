41c0d558fbbf92f3bbb732c44a781aab6d5a362d
hellogaray <137473678+hellogaray@users.noreply.github.com>
2025-04-01 10:00:00 +0900

    Reorganizing

--- Problems/1431. Kids With the Greatest Number of Candies.py
+++ Problems/1431. Kids With the Greatest Number of Candies.py
@@ -17,10 +17,5 @@ Example:
 """
 class Solution(object):
     def kidsWithCandies(self, candies, extraCandies):
-        """
-        :type candies: List[int]
-        :type extraCandies: int
-        :rtype: List[bool]
-        """
         max_candies = max(candies)
         return [i + extraCandies >= max_candies for i in candies]
