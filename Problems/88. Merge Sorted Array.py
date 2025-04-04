271965156d8229e4f13a57b58868d570d36f4c32
hellogaray <137473678+hellogaray@users.noreply.github.com>
2025-04-04 10:00:00 +0900

    Reorganizing

--- Problems/88. Merge Sorted Array.py
+++ Problems/88. Merge Sorted Array.py
@@ -27,26 +27,3 @@ def merge(nums1, m, nums2, n):
             nums1[k] = nums2[j]
             j -= 1
         k -= 1
-
-
-
-
-nums1 = [1,2,3,0,0,0]
-m = 3
-nums2 = [2,5,6]
-n = 3
-# Output: [1,2,2,3,5,6]
-
-nums1 = [4,5,6,0,0,0]
-m = 3
-nums2 = [1,2,3]
-n = 3
-# Output: [1]
-print(merge(nums1, m, nums2, n))
-
-nums1 = [0]
-m = 0
-nums2 = [1]
-n = 1
-# Output: [1]
-print(merge(nums1, m, nums2, n))
