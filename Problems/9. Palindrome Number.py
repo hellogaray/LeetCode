28460d528546ecab78d77c38015a8aaeab8ac32f
hellogaray <137473678+hellogaray@users.noreply.github.com>
2025-04-06 10:00:00 +0900

    Reorganizing

--- Problems/9. Palindrome Number.py
+++ Problems/9. Palindrome Number.py
@@ -16,9 +16,3 @@ class Solution:
         if y == y[::-1]:
             return True
         return False
-
-
-solution = Solution()
-print(solution.isPalindrome(121))  # Output: True
-print(solution.isPalindrome(-121)) # Output: False
-print(solution.isPalindrome(10))   # Output: False
