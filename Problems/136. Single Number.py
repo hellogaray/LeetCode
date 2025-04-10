98d9543de091c80ca405cdaf2121d1022c149b47
hellogaray <137473678+hellogaray@users.noreply.github.com>
2025-04-10 10:00:00 +0900

    Reorganizing

--- Problems/136. Single Number.py
+++ Problems/136. Single Number.py
@@ -12,5 +12,4 @@ Example 1:
     Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
     The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
 """
-# Output: [1]
-n = 3
+
