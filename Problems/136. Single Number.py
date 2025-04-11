ee4660a6b8ad4479acd54bae205b9cf2e561a9df
hellogaray <137473678+hellogaray@users.noreply.github.com>
2025-04-11 10:00:00 +0900

    Reorganizing

--- Problems/136. Single Number.py
+++ Problems/136. Single Number.py
@@ -12,41 +12,5 @@ Example 1:
     Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
     The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
 """
-
-def merge(nums1, m, nums2, n):
-    i = m - 1
-    j = n - 1
-    k = m + n - 1
-
-    while j >= 0:
-        print(nums1, nums2, i, j, k)
-        if i >= 0 and nums1[i] > nums2[j]:
-            nums1[k] = nums1[i]
-            i -= 1
-        else:
-            nums1[k] = nums2[j]
-            j -= 1
-        k -= 1
-
-
-
-
-nums1 = [1,2,3,0,0,0]
-m = 3
-nums2 = [2,5,6]
-n = 3
-# Output: [1,2,2,3,5,6]
-
-nums1 = [4,5,6,0,0,0]
-m = 3
-nums2 = [1,2,3]
-n = 3
 # Output: [1]
-print(merge(nums1, m, nums2, n))
-
-nums1 = [0]
-m = 0
-nums2 = [1]
-n = 1
-# Output: [1]
-print(merge(nums1, m, nums2, n))
+n = 3
