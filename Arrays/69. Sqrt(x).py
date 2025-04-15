c298bff8be5e74f13a5d231dd99cb15911b305eb
hellogaray <137473678+hellogaray@users.noreply.github.com>
2025-04-15 21:29:26 +0900

    Problem 69 - Arrays

--- Arrays/69. Sqrt(x).py
+++ Arrays/69. Sqrt(x).py
@@ -10,10 +10,19 @@ Example 1:
 """
 
 
-def mySqrt(x):
-    ...
+lass Solution:
+    def mySqrt(self, x: int) -> int:
+        if x < 2:
+            return x
 
-x = 4
-print(mySqrt(x))
-x = 8
-print(mySqrt(x))
+        start, end, answer = 0, (x//2) , 0
+        while start <= end:
+            mid = ((start + end) // 2)
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                answer = mid
+                start = mid + 1
+            else:
+                end = mid - 1
+        return answer
